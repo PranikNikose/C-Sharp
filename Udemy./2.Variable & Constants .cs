3c9d0921da221bbacf614f754da9c4af2a9dfcd2
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-06-17 12:18:25 +0530

    Update 2.Variable & Constants .cs

--- Udemy./2.Variable & Constants .cs
+++ Udemy./2.Variable & Constants .cs
@@ -1,1 +1,23 @@
-
+using System;
+namespace VariableConstantNS
+{
+  class VariableConstantClass
+  {
+  static void Main(string[] argv)
+    {
+   byte b = 1;
+   int number =10;
+   float f = 20.95f;
+   char c = 'P';
+   string s = "Pranik";
+   bool isWorking =true;
+   
+   Console.WriteLine("byte:{0}",b);
+   Console.WriteLine("int:{0}",number);
+   Console.WriteLine("float:{0}",f);
+   Console.WriteLine("char:{0}",c);
+   Console.WriteLine("string:{0}",s);
+   Console.WriteLine("bool:{0}",isWorking);
+   }
+  }
+}
