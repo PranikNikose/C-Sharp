ee11eef07960a0a407525d58e14e7c35d85cb194
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-06-18 00:14:30 +0530

    Create 11. Value & Reference Types .cs

---
+++ Udemy./11. Value & Reference Types .cs
@@ -0,0 +1,23 @@
+using System;
+namespace ReferenceValueType
+{
+    class RVTClass
+    { 
+        static void Main()
+        {   
+            //Value Type 
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(string.Format("A: {0} B: {1}",a,b));
+            Console.WriteLine();
+
+
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine(string.Format("Array1: {0} Array2: {1}",array1[0],array2[0]));
+            Console.WriteLine();
+        }
+    }
+}
