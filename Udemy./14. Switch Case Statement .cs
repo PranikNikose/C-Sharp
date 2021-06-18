b32b9f82e51bd1aa23103d5f53bf991a68ddf37d
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-06-18 13:46:33 +0530

    Update 14. Switch Case Statement .cs

--- Udemy./14. Switch Case Statement .cs
+++ Udemy./14. Switch Case Statement .cs
@@ -1,10 +1,18 @@
 using System;
 namespace SwitchCaseNS
-{
+{   
+    public enum Season
+    {
+        Summer,
+        Winter,
+        Rainy
+    }
+
     class SwitchCaseClass
     {
     static void Main()
         {
+            Console.WriteLine();
             string role = "Admin"; 
             switch (role)
             {
@@ -13,9 +21,26 @@ namespace SwitchCaseNS
                     break;
 
                 case "Mod":
-                    Console.WriteLine("I am Moderator");
+                    Console.WriteLine("Moderator there");
                     break;
             }
+            Console.WriteLine();
+
+            var s1 = Season.Summer;
+            switch (s1)
+            {
+                case Season.Summer:
+                    Console.WriteLine("This is Summer");
+                    break;
+                case Season.Winter:
+                    Console.WriteLine("This is Winter");
+                    break;
+                case Season.Rainy:
+                    Console.WriteLine("This is Rainy");
+                    break;
+            }
+            Console.WriteLine();
+
         }
     }
 }
