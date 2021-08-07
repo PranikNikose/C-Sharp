2195c7db8c381833a469270098594fdcd2fe98ed
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-08-07 12:44:09 +0530

    Create Exercise3.cs
    Write a C# Sharp program to print the result of dividing two numbers.

---
+++ w3resource.com/Basic Exercise/Exercise3.cs
@@ -0,0 +1,11 @@
+using System;
+public class Exercise3
+{
+    public static void Main()
+    {
+        int FN = 10;
+        int SN = 2;
+        Console.WriteLine("Result of Division = {0}",FN/SN);
+       
+    }
+}
