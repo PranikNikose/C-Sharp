a18cf53e7a909e25861a7b45dd871893dbd385f9
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-08-07 19:48:51 +0530

    Create Exercise25.cs
    Write a C# program to print the odd numbers from 1 to 99. Prints one number per line.

---
+++ w3resource.com/Basic Exercise/Exercise25.cs
@@ -0,0 +1,15 @@
+using System;
+namespace skd
+{
+    public class Exercise25
+    {
+        public static void Main()
+        {
+            int i;
+            for (i = 1; i < 100; i=i+2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
