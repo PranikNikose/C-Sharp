ba97b1787df202490a7fca8bc63227604db5a0d4
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-08-07 12:41:42 +0530

    Create Exercise2.cs

---
+++ w3resource.com/Basic Exercise/Exercise2.cs
@@ -0,0 +1,11 @@
+using System;
+public class Exercise1
+{
+    public static void Main()
+    {
+        int FN = 10;
+        int SN = 20;
+        Console.WriteLine("Sum = {0}",FN+SN);
+       
+    }
+}
