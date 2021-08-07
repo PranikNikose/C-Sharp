3fab60f1f7e12daddd9f01d6852745d2a1a2a19d
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-08-07 13:02:34 +0530

    Create Exercise9.cs
     Write a C# Sharp program that takes four numbers as input to calculate and print the average.

---
+++ w3resource.com/Basic Exercise/Exercise9.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Exercise9
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the First Number:");
+        int Fn = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the First Number:");
+        int Sn = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the First Number:");
+        int Tn = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the First Number:");
+        int Frn = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Avrage :{0}",(Fn+Sn+Tn+Frn)/4);
+        
+    }
+}
