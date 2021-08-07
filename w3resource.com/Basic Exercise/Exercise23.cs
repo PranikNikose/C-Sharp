024496e847167564bb880ecf6a956b7071d0eb54
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-08-07 19:34:29 +0530

    Create Exercise23.cs
    Write a C# program to convert a given string into lowercase.

---
+++ w3resource.com/Basic Exercise/Exercise23.cs
@@ -0,0 +1,17 @@
+using System;
+namespace skd
+{
+    public class Exercise23
+    {
+        public static void Main()
+        {
+            string str = "Write a C# Sharp Program to display the following pattern using the alphabet.";
+             Console.WriteLine(ToLowerstr(str));
+        }
+
+        public static string ToLowerstr(string str)
+        {
+            return str.ToLower();
+        }
+    }
+}
