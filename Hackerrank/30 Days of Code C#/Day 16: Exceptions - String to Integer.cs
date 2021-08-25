59528df602a606c297f2a239bec589134cd3aa02
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-08-25 14:38:23 +0530

    Create Day 16: Exceptions - String to Integer.cs

---
+++ Hackerrank/30 Days of Code C#/Day 16: Exceptions - String to Integer.cs
@@ -0,0 +1,28 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        try{
+             string S = Console.ReadLine();
+            Console.WriteLine(int.Parse(S));
+        }
+       catch (FormatException fe)
+       {
+           Console.WriteLine("Bad String");
+       }
+    }
+}
