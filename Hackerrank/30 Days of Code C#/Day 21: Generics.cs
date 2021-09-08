c2d85b53ad6c2ef50a539abf5b6624deaf2ee013
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2021-09-08 13:38:45 +0530

    Create Day 21: Generics.cs

---
+++ Hackerrank/30 Days of Code C#/Day 21: Generics.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Printer
+{
+    static void PrintArray <Element> (Element []arr)
+    {
+        foreach(var arrele in arr) 
+        {
+            Console.WriteLine(arrele);
+        }
+    }
+	
+
+
+    static void Main(string[] args)
+	{
+		int n = Convert.ToInt32(Console.ReadLine());
+		int[] intArray = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			intArray[i] = Convert.ToInt32(Console.ReadLine());
+		}
+		
+		n = Convert.ToInt32(Console.ReadLine());
+		string[] stringArray = new string[n];
+		for (int i = 0; i < n; i++)
+		{
+			stringArray[i] = Console.ReadLine();
+		}
+		
+		PrintArray<Int32>(intArray);
+		PrintArray<String>(stringArray);
+	}
+}
